8cdbbf7b44a10bd396e831ba26b313485cd47a67
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-11-09 18:47:34 +0600

    Refactor MusicsPageLayout component and add BanglaSongList component

--- app/(pages)/bangla-song/layout.tsx
+++ app/(pages)/bangla-song/layout.tsx
@@ -1,12 +1,16 @@
 import MainCard from "@/components/ui/main-card/MainCard";
 import React from "react";
+import styles from "@/components/ui/main-card/MainCard.module.scss";
 
-const MusicsPageLayout = ({
-    children
-} : {
-    children: React.ReactNode
-}) => {
-    return <MainCard title="All Musics">{children}</MainCard>
-}
+const MusicsPageLayout = ({ children }: { children: React.ReactNode }) => {
+  const classes = `${styles.card} `;
+  return (
+    <div className={classes}>
+      {/* <div className={styles.header}>{template}</div> */}
+
+      {children}
+    </div>
+  );
+};
 
 export default MusicsPageLayout;
--- app/(pages)/bangla-song/page.tsx
+++ app/(pages)/bangla-song/page.tsx
@@ -1,14 +1,16 @@
+import BanglaSongList from "@/components/bangla/BanglaSongList";
 import Musics from "@/components/msuics/Musics";
-import GetMusics from "@/services/server/musics/GetMusics";
 
 export const metadata = {
-    title: "Musics"
-}
+  title: "Musics",
+};
 
 const MusicsPage = async () => {
-    const musics : Music[] = await GetMusics()
-    
-    return <Musics musics={musics} />
-}
+  return (
+    <>
+      <BanglaSongList />
+    </>
+  );
+};
 
 export default MusicsPage;
