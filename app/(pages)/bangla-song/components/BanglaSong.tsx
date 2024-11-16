3cac4cf239f771b123d4ec12e4e3012cacea4be4
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-11-16 21:11:52 +0600

    Refactor BanglaSong component and MusicList styles

--- app/(pages)/bangla-song/components/BanglaSong.tsx
+++ app/(pages)/bangla-song/components/BanglaSong.tsx
@@ -6,33 +6,33 @@ import MusicItem from '@/components/music/MusicItem';
 
 const BanglaSong = () => {
 
-    const PLAY_LIST_ID = "top-charts-musics-playlist";
+  const PLAY_LIST_ID = "top-charts-musics-playlist";
 
-    const [setMusic, setPlaylist, id] = useAppStore((state) => [
-      state.setMusic,
-      state.setPlaylist,
-      state.playListId,
-    ]);
-  
-    const musicClickHandler = (music: Music) => {
-      setMusic(music);
-      if (id !== PLAY_LIST_ID) {
-        setPlaylist(PLAY_LIST_ID, musicList?.music);
-      }
-    };
+  const [setMusic, setPlaylist, id] = useAppStore((state) => [
+    state.setMusic,
+    state.setPlaylist,
+    state.playListId,
+  ]);
 
-    
-    return (
-        <>
-           {musicList?.music?.map((music) => (
-          <MusicItem
-            key={music.id}
-            musicData={music}
-            onMusicClick={musicClickHandler}
-          />
-        ))} 
-        </>
-    )
+  const musicClickHandler = (music: Music) => {
+    setMusic(music);
+    if (id !== PLAY_LIST_ID) {
+      setPlaylist(PLAY_LIST_ID, musicList?.music);
+    }
+  };
+
+
+  return (
+    <>
+      {musicList?.music?.map((music) => (
+        <MusicItem
+          key={music.id}
+          musicData={music}
+          onMusicClick={musicClickHandler}
+        />
+      ))}
+    </>
+  )
 }
 
 export default BanglaSong
