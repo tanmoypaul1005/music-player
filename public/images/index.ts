993d837c74b3e4853c395585b92fc921c5651798
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-11-17 13:54:19 +0600

    Remove unused notFoundMusicImage import from index.ts

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,7 +1,6 @@
 import logo from './logo.webp'
 import notFoundImage from './not-found.webp'
 import notFoundPageImage from './404.webp'
-import notFoundMusicImage from './not-found-music.webp'
 
 import artistsBackgroundImage from './background/artists.webp'
 
@@ -9,6 +8,5 @@ export {
     logo,
     notFoundImage,
     notFoundPageImage,
-    notFoundMusicImage,
     artistsBackgroundImage,
 }
